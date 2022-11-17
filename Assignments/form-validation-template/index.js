456af972c5e057b6957c242c4c84c11037bd5d8d
richycpcc <113448538+richycpcc@users.noreply.github.com>
2022-11-17 13:20:53 -0500

    correct

--- Assignments/form-validation-template/index.js
+++ Assignments/form-validation-template/index.js
@@ -56,46 +56,65 @@ const validateForm = (event) => {
         //Loop a single input through the vaildation errors on the form
         for (const input of inputs){
         const errorList = [];    
-         
-            if(input.classList.contains('required')){
-                !isRequiredValid(input);
-                errorList.push('Required fields must have a value that is not empty or whitespace.');
-                console.log(input.value);
-                console.log(errorList);
+            
+            //if statements - match to class
+            if(input.classList.value === ('required')){ //getElementsByClassName()
+                if (isRequiredValid(input) === false){
+                    errorList.push('Required fields must have a value that is not empty or whitespace.');
+                    console.log(input.value +);
+                    console.log(errorList);
+                }
             }
-            if(input.classList = 'alphabetic'){
-                !isAlphabeticValid(input);
-                errorList.push('Alphabetic fields must be a series of alphabetic characters.');
-                //return;
+
+            if(input.classList == 'alphabetic'){
+                 if(isAlphabeticValid(input) === false){
+                    errorList.push('Alphabetic fields must be a series of alphabetic characters.');
+                 };
             }
-            if(input.classList = 'username'){//need to add length error check as well
-                if (!isUsernameValid(input)){
+
+            if(input.classList == 'username'){//need to add length error check as well
+                if (isUsernameValid(input) === false){
                     errorList.push('Username fields must contain only alphanumeric characters.');
                 }
-                else if (!isLengthValid(input)){
+                else if (isLengthValid(input)===false){
                     errorList.push('Username fields must contain at least 8 characters.');
                 };
             }
-            if(input.classList = 'password'){
-                !isPasswordValid(input);
+
+            if(input.classList === 'password'){
+                if (isPasswordValid(input)===false){
                 errorList.push('Password fields must contain one or more of each of the following types:uppercase letters, lowercase letters, numbers, special characters.');
+                }
             }
-            if(input.classList = 'required_size'){
-                !isLengthValid(input);
+
+            if(input.classList === 'required_size'){
+                if (isLengthValid(input)===false){
                 errorList.push('Required_size field lengths must exactly match the minlength attribute of that field.');
+                }
             }
-            if(input.classList = 'phone'){
-                !isphoneValid(input);
+
+            if(input.classList === 'phone'){
+                if (isphoneValid(input)===false){
                 errorList.push('Phone fields must match the format of XXX-XXX-XXXX.');
+                }
             }
-            if(input.classList = 'date'){
-                !isDateValid(input);
+
+            if(input.classList === 'date'){
+                if (isDateValid(input) === false){
                 errorList.push('Date fields must match the format of XX/XX/XXXX.');
+                }
             }
+
+            if(input.classList === 'numeric'){
+                if (isNumberValid(input) === false){
+                errorList.push('Numeric fields must be a series of numbers.');
+                }
+            }
+            /*//proof of how dumb I am :)
             if(input.classList = 'numeric'){
                 !isNumberValid(input);
                 errorList.push('Numeric fields must be a series of numbers.');
-            }
+            */
         //} 
 
             if(errorList.length > 0){
@@ -104,7 +123,7 @@ const validateForm = (event) => {
                 //writeErrorList(errorList);
                 document.querySelector('.errors').innerHTML = errorList.join(',');
                 document.querySelector('.errors').style="color:red";
-                //console.log(errorList);        }
+                console.log(errorList);        
 
             } //end if
 

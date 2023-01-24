4a926f874d73a135af938b7c01b256e976b1ce79
richycpcc <113448538+richycpcc@users.noreply.github.com>
2023-01-24 10:58:33 -0500

    adding Springboot Demo

---
+++ Lecture Work/Module 4 - Web Servers/Spring Boot Demo/src/main/java/io/catalyte/training/springboot/DemoRunner.java
@@ -0,0 +1,6 @@
+package io.catalyte.training.springboot;
+
+public class DemoRunner
+{
+    public static void main(String[]args){}
+}
---
+++ Lecture Work/Module 4 - Web Servers/Spring Boot Demo/src/main/java/io/catalyte/training/springboot/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package io.catalyte.training.springboot.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+    @RestController
+    public class HomeController
+    {
+        @RequestMapping(value = "/", method = RequestMethod.GET)
+        public String helloWorld()
+        {
+            return"Hello World!";
+        }
+    }
+
